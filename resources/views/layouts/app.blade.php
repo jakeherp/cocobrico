833cdbbf12f32a1eb680e2190bf6873702123262
Robert Schlick <robert.schlick@online.de>
2016-01-14 16:53:49 +0200

    Corrected paths.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,15 +5,15 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Cocobrico Commercial</title>
-    <link rel="stylesheet" href="css/foundation.css" />
+	<link rel="stylesheet" href="{{ URL::asset('css/foundation.css') }}" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/app.css" />
+	<link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" />
   </head>
   <body>
 
     <header class="row">
       <div class="large-12 columns text-center">
-        <h1><img src="img/logo.svg" alt="Cocobrico">Commercial Customers</h1>
+        <h1><img src="{{ URL::asset('img/logo.svg') }}" alt="Cocobrico">Commercial Customers</h1>
       </div>
     </header>
 
@@ -24,11 +24,10 @@
     	&copy; 2016 Cocobrico Europe Ltd
       </div>
     </footer>
-
-
-    <script src="js/vendor/jquery.min.js"></script>
-    <script src="js/vendor/what-input.min.js"></script>
-    <script src="js/foundation.min.js"></script>
-    <script src="js/app.js"></script>
+	
+	<script type="text/javascript" src="{{ URL::asset('js/vendor/jquery.min.js') }}"></script>
+	<script type="text/javascript" src="{{ URL::asset('js/vendor/what-input.min.js') }}"></script>
+	<script type="text/javascript" src="{{ URL::asset('js/foundation.min.js') }}"></script>
+	<script type="text/javascript" src="{{ URL::asset('js/app.js') }}"></script>
   </body>
 </html>
