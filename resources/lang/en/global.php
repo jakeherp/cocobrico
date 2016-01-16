52ad7d3977162ab377507883b55be371e30d3787
Jacob Herper <jacob@pcserve-media.com>
2016-01-16 12:34:04 +0000

    Localisation of template

---
+++ resources/lang/en/global.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Global Language Lines
+    |--------------------------------------------------------------------------
+	*/
+
+    'title'				=> 'Cocobrico Commercial',
+    'menu'				=> 'Menu',
+    'dashboard'			=> 'Dashboard',
+    'orders'				=> 'Orders',
+    'accounts'			=> 'Accounts',
+    'downloads'			=> 'Downloads',
+    'settings'			=> 'Settings',
+    'logout'				=> 'Logout',
+
+];
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Cocobrico Commercial</title>
+    <title>{{ trans('global.title') }}</title>
 	<link rel="stylesheet" href="{{ URL::asset('css/foundation.css') }}" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" />
@@ -18,22 +18,22 @@
 
       <div class="title-bar" data-responsive-toggle="main-nav" data-hide-for="medium">
         <button class="menu-icon" type="button" data-toggle></button>
-        <div class="title-bar-title">Menu</div>
+        <div class="title-bar-title">{{ trans('global.menu') }}</div>
       </div>
 
       <div class="top-bar-left" id="main-nav">
         <ul class="menu">
-          <li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
-          <li><a href="orders.php"><i class="fa fa-truck"></i> Orders</a></li>
-          <li><a href="accounts.php"><i class="fa fa-usd"></i> Accounts</a></li>
-          <li><a href="downloads.php"><i class="fa fa-download"></i> Downloads</a></li>
-          <li><a href="settings.php"><i class="fa fa-cog"></i> Settings</a></li>
+          <li><a href="dashboard.php"><i class="fa fa-dashboard"></i> {{ trans('global.dashboard') }}</a></li>
+          <li><a href="orders.php"><i class="fa fa-truck"></i> {{ trans('global.orders') }}</a></li>
+          <li><a href="accounts.php"><i class="fa fa-usd"></i> {{ trans('global.accounts') }}</a></li>
+          <li><a href="downloads.php"><i class="fa fa-download"></i> {{ trans('global.downloads') }}</a></li>
+          <li><a href="settings.php"><i class="fa fa-cog"></i> {{ trans('global.settings') }}</a></li>
         </ul>
       </div>
       <div class="top-bar-right">
         <ul class="dropdown menu" data-dropdown-menu>
-          <li>Welcome back, Mr. Reichmuth</li>
-          <li class="logout"><a href="index.php"><i class="fa fa-sign-out"></i> Logout</a></li>
+          <li>{{ trans('global.welcome') }}, {{user}}</li>
+          <li class="logout"><a href="index.php"><i class="fa fa-sign-out"></i> {{ trans('global.logout') }}</a></li>
         </ul>
       </div>
     </header>
@@ -42,7 +42,7 @@
 	    
     <footer class="row">
       <div class="large-12 columns text-center">
-    	&copy; 2016 Cocobrico Europe Ltd
+    	&copy; <?php echo date("Y"); ?> Cocobrico Europe Ltd
       </div>
     </footer>
 
