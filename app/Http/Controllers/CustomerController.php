0028b3421a441901ba5e3506abdb13e2839b05c8
Robert Schlick <robert.schlick@online.de>
2016-01-28 01:40:38 +0200

    Bugfix Register

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -47,7 +47,7 @@ class CustomerController extends Controller
 		$customer->billingEmail = $user->email;
 		$customer->taxID = $request->taxID;
 		$user->customers()->save($customer);
-		return view('pages.dashboard');
+		return view('pages.dashboard',compact('user'));
 	}
 
 	/**
