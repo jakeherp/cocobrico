6ad28b3cb420a4fddb75386811fdd62701cfbf61
Jacob Herper <jacob@pcserve-media.com>
2016-02-06 18:51:44 +0100

    Email Template changes v2

--- resources/views/emails/verifyEmail.blade.php
+++ resources/views/emails/verifyEmail.blade.php
@@ -1,10 +1,14 @@
 <body style="background: #000;">
-	<img src="{{ URL::asset('img/logo.svg') }}" alt="Cocobrico" style="width: 300px; display: block; margin: 0 auto 30px">
-	<div style="background: #fff; color: #000; padding: 20px; width: 600px; margin: 20px auto;">
+	<img src="{{ URL::asset('img/logo.png') }}" alt="Cocobrico" style="width: 90%; max-width: 300px; display: block; margin: 0 auto 30px">
+	<div style="background: #fff; color: #000; padding: 20px; width: 90%; margin: 20px auto;">
 		<p>Dear customer,</p>
 		<p>Please verify your email address to continue your registration with Cocobrico Commercial Customers:</p>
 
-		<p style="text-align: center;"><span style="background: #ee1d23; padding: 10px; color: #fff; font-size: 1.25em;">{!! link_to('http://cb.pcserve.eu/register/'.$user->register_token, $title = 'Click here to verify your email address', $attributes = array(), $secure = null); !!}</span></p>
+		<p style="text-align: center; margin: 30px 0;">
+			<span style="background: #ee1d23; padding: 10px; color: #fff; font-size: 1.25em;">
+				{!! link_to('http://cb.pcserve.eu/register/'.$user->register_token, $title = 'Click here to verify your email address', $attributes = array(), $secure = null); !!}
+			</span>
+		</p>
 
 		<p>If you have trouble activating your account, please contact us at info@cocobrico.com</p>
 	</div>
