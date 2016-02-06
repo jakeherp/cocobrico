a356d91eb33717439cf47124213d517b13d81233
Jacob Herper <jacob@pcserve-media.com>
2016-02-06 18:59:35 +0100

    Verify Page changes

--- resources/views/auth/verifyEmail.blade.php
+++ resources/views/auth/verifyEmail.blade.php
@@ -5,9 +5,10 @@
 	<section class="row" id="login">
       <div class="large-6 small-12 large-centered columns">
         <div class="callout large">
-          <h3>Verify your Email Adress</h3>
-          <p>We sent an email to {{ $user->email }}. Please verify it by clicking on the link it contains. If you
-          	havn't got a email, check your spam directory or click this LINK to send a new one.</p>
+          <h3>Verify your email address</h3>
+          <div class="success callout">
+            <p>We have sent a confirmation link to {{ $user->email }}. Please verify your email address to proceed the registration process. In case you have not received an email, please check your junk folder or click <a href="#">here</a> to resend the confirmation email.</p>
+          </div>
         </div>
       </div>
     </section>
