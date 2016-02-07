62cb8ef643541cc0187e538e607f01ce667e0cd2
Jacob Herper <jacob@pcserve-media.com>
2016-02-07 16:41:56 +0100

    Refactoring Admin Template / Error fixing

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -14,79 +14,69 @@
   <body>
 
 
-<div class="off-canvas-wrapper">
+    <div class="off-canvas-wrapper">
 
-    <!-- off-canvas title bar -->
-    <div class="title-bar">
+      <div class="title-bar">
 
-      <div class="top-bar-left">
-        <button class="menu-icon" type="button" data-open="offCanvasLeft"></button>
-        <img src="{{ URL::asset('img/logo.svg') }}" alt="Cocobrico"> <span>Administration Panel</span>
-      </div>
+        <div class="top-bar-left">
+          <button class="menu-icon" type="button" data-open="offCanvasLeft"></button>
+          <img src="{{ URL::asset('img/logo.svg') }}" alt="Cocobrico"> <span>Administration Panel</span>
+        </div>
 
-
-      <div class="title-bar-right">
-
-        <ul class="menu">
-          <li><a href="{{ url('admin/logout') }}"><i class="fa fa-sign-out"></i> Logout</a></li>
-        </ul>
+        <div class="title-bar-right">
+          <ul class="menu">
+            <li><a href="{{ url('admin/logout') }}"><i class="fa fa-sign-out"></i> Logout</a></li>
+          </ul>
+        </div>
 
       </div>
 
-    </div>
 
+      <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
 
-  <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
+        <div class="off-canvas position-left" id="offCanvasLeft" data-off-canvas>
 
+          <ul class="vertical dropdown menu" data-dropdown-menu>
+            <li class="user">Andi Reichmuth<em>Administrator</em></li>
 
-    <!-- off-canvas left menu -->
+            <li><a href="{{ url('admin/dashboard') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>        
+            <li><a href="{{ url('admin/customers') }}"><i class="fa fa-user"></i> Customers</a></li>
+            <li><a href="{{ url('admin/warehouses') }}"><i class="fa fa-building"></i> Warehouses</a></li>
+            <li><a href="{{ url('admin/countries') }}"><i class="fa fa-globe"></i> Exclusivities</a></li>
+            <li><a href="{{ url('admin/statistics') }}"><i class="fa fa-bar-chart"></i> Statistics</a></li>
 
-    <div class="off-canvas position-left" id="offCanvasLeft" data-off-canvas>
+            <li class="title"><i class="fa fa-tags"></i> Orders</li>
+              <li><a href="{{ url('admin/orders/options') }}"><i class="fa fa-tag"></i> Options</a></li>
+              <li><a href="{{ url('admin/orders/containers') }}"><i class="fa fa-ship"></i> Containers</a></li>
+              <li><a href="{{ url('admin/orders/pallets') }}"><i class="fa fa-truck"></i> Pallets</a></li>
 
-      <ul class="vertical dropdown menu" data-dropdown-menu>
+            <li class="title"><i class="fa fa-cog"></i> Settings</li>
+              <li><a href="{{ url('admin/announcements') }}"><i class="fa fa-warning"></i> Announcements</a></li>
+              <li><a href="{{ url('admin/users') }}"><i class="fa fa-user-secret"></i> Users</a></li>
+              <li><a href="{{ url('admin/userroles') }}"><i class="fa fa-users"></i> User Roles</a></li>
+              <li><a href="{{ url('admin/prices') }}"><i class="fa fa-usd"></i> Prices</a></li>
+          </ul>
 
-        <li class="user">Andi Reichmuth<em>Administrator</em></li>
-            
-        <li><a href="{{ url('admin/dashboard') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>        
-        <li><a href="{{ url('admin/customers') }}"><i class="fa fa-user"></i> Customers</a></li>
-        <li><a href="{{ url('admin/warehouses') }}"><i class="fa fa-building"></i> Warehouses</a></li>
-        <li><a href="{{ url('admin/countries') }}"><i class="fa fa-globe"></i> Exclusivities</a></li>
-        <li><a href="{{ url('admin/statistics') }}"><i class="fa fa-bar-chart"></i> Statistics</a></li>
+        </div>
 
-        <li class="title"><i class="fa fa-tags"></i> Orders</li>
-            <li><a href="{{ url('admin/orders/options') }}"><i class="fa fa-tag"></i> Options</a></li>
-            <li><a href="{{ url('admin/orders/containers') }}"><i class="fa fa-ship"></i> Containers</a></li>
-            <li><a href="{{ url('admin/orders/pallets') }}"><i class="fa fa-truck"></i> Pallets</a></li>
-
-        <li class="title"><i class="fa fa-cog"></i> Settings</li>
-            <li><a href="{{ url('admin/announcements') }}"><i class="fa fa-warning"></i> Announcements</a></li>
-            <li><a href="{{ url('admin/users') }}"><i class="fa fa-user-secret"></i> Users</a></li>
-            <li><a href="{{ url('admin/userroles') }}"><i class="fa fa-users"></i> User Roles</a></li>
-            <li><a href="{{ url('admin/prices') }}"><i class="fa fa-usd"></i> Prices</a></li>
-
-      </ul>
-
-    </div>
-
-    <div class="off-canvas-content" data-off-canvas-content>
+        <div class="off-canvas-content" data-off-canvas-content>
           <div class="row column">
 
+            <section class="row" id="content">
+              @yield('content')
+            </section>
 
-    
-    <section class="row" id="content">
-	   @yield('content')
-    </section>
-    
+            <footer class="row">
+              <div class="large-12 columns text-center">
+                &copy; 2016 Cocobrico Europe Ltd
+              </div>
+            </footer>
 
-    <footer class="row">
-      <div class="large-12 columns text-center">
-      &copy; 2016 Cocobrico Europe Ltd
+          </div>
+        </div>
+
       </div>
-    </footer>
-      </div>
-
     </div>
-
 
   <script type="text/javascript" src="{{ URL::asset('js/vendor/what-input.min.js') }}"></script>
   <script type="text/javascript" src="{{ URL::asset('js/foundation.min.js') }}"></script>
