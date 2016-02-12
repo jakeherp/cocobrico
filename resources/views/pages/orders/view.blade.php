069dc91723d01918bfb2e15ba123194140b396eb
Jacob Herper <jacob@pcserve-media.com>
2016-02-12 17:23:23 +0000

    Order Localisation

--- resources/views/pages/orders/view.blade.php
+++ resources/views/pages/orders/view.blade.php
@@ -5,34 +5,34 @@
     <section class="row" id="content">
 
     <div class="large-12 column">        
-      <h1><i class="fa fa-truck"></i> Order {{ $pallet->customerReference }}</h1>
+      <h1><i class="fa fa-truck"></i> {{ trans('orders.order') }} {{ $pallet->customerReference }}</h1>
     </div>
     
     <div class="large-8 medium-6 small-12 column">
       <div class="callout">
         <div class="row">
           <div class="medium-6 small-12 columns">
-            <h5>Billing Address</h5>
+            <h5>{{ trans('orders.billingaddress') }}</h5>
             <strong>{{ $identity->getBillingAddress()->companyName }}</strong><br>
             {{ $identity->getBillingAddress()->address1 }}<br>
             {{ $identity->getBillingAddress()->postCode }} {{ $identity->getBillingAddress()->city }}<br>
             {{ $identity->getBillingAddress()->country }}<br>
             <br>
-            Phone: {{ $identity->getBillingAddress()->phone }}<br>
-            Email: {{ $identity->getBillingAddress()->email }}
+            {{ trans('orders.phone') }}: {{ $identity->getBillingAddress()->phone }}<br>
+            {{ trans('orders.email') }}: {{ $identity->getBillingAddress()->email }}
           </div>
           <div class="medium-6 small-12 columns">
             @if($pallet->address_id != 0)
-              <h5>Shipping Address</h5>
+              <h5>{{ trans('orders.shippingaddress') }}</h5>
               <strong>{{ $pallet->address->companyName }}</strong><br>
               {{ $pallet->address->address1 }}<br>
               {{ $pallet->address->postCode }} {{ $pallet->address->city }}<br>
               {{ $pallet->address->country }}<br>
               <br>
-              Phone: {{ $pallet->address->phone }}<br>
-              Email: {{ $pallet->address->email }}
+              {{ trans('orders.phone') }}: {{ $pallet->address->phone }}<br>
+              {{ trans('orders.email') }}: {{ $pallet->address->email }}
             @else
-              <h5>Pickup from Warehouse</h5>
+              <h5>{{ trans('orders.pickup') }}</h5>
             @endif
           </div>
         </div>
@@ -40,34 +40,34 @@
         <hr>
 
         <div class="large-12">
-          <h5>General Details</h5>
+          <h5>{{ trans('orders.details') }}</h5>
           <table class="scroll" style="width: 100%;">
             <tbody>
               <tr>
-                <td>Order Date</td>
+                <td>{{ trans('orders.date') }}</td>
                 <td>{{ date('d.m.Y', time($pallet->created_at)) }}</td>
               </tr>
               <tr>
-                <td>Order Time</td>
+                <td>{{ trans('orders.time') }}</td>
                 <td>{{ date('H:i:s', time($pallet->created_at)) }}</td>
               </tr>
               <tr>
-                <td>Order Status</td>
-                <td>Invoice sent</td>
+                <td>{{ trans('orders.status') }}</td>
+                <td>{{ trans('orders.billed') }}</td>
               </tr>
             </tbody>
           </table>
         </div>
       </div>
       <div class="callout">
-        <h5>Ordered Items</h5>
+        <h5>{{ trans('orders.items') }}</h5>
           <table style="width: 100%;">
             <thead>
               <tr>
-                <th>Item</th>
-                <th>PPU</th>
-                <th>Quantity</th>
-                <th>Total</th>
+                <th>{{ trans('orders.item') }}</th>
+                <th>{{ trans('orders.ppu') }}</th>
+                <th>{{ trans('orders.quantity') }}</th>
+                <th>{{ trans('orders.total') }}</th>
               </tr>
             </thead>
             <tbody>
@@ -84,25 +84,25 @@
               <tr>
                 <td></td>
                 <td></td>
-                <td>Shipping from Darmstadt:</td>
+                <td>{{ trans('orders.shipping') }}:</td>
                 <td>150,00 EUR</td>
               </tr>
               <tr>
                 <td></td>
                 <td></td>
-                <td>Order Total:</td>
+                <td>{{ trans('orders.order') }} {{ trans('orders.total') }}:</td>
                 <td>{{ $pallet->get_total_price() }} EUR</td>
               </tr>
               <tr>
                 <td></td>
                 <td></td>
-                <td>Paid:</td>
+                <td>{{ trans('orders.paid') }}:</td>
                 <td>0,00 EUR</td>
               </tr>
               <tr>
                 <td></td>
                 <td></td>
-                <td>Balance due:</td>
+                <td>{{ trans('orders.due') }}:</td>
                 <td>{{ $pallet->get_total_price() }} EUR</td>
               </tr>
             </tfoot>
@@ -112,19 +112,19 @@
 
     <div class="large-4 medium-6 small-12 column">
       <div class="callout">
-        <h5>Options</h5>
-        <a class="small expanded success button" href="#"><i class="fa fa-copy"></i> Place order again</a>
-        <a class="small expanded warning button" href="#"><i class="fa fa-pencil"></i> Change order</a>
-        <a class="small expanded alert button" href="#"><i class="fa fa-trash"></i> Cancel order</a>
+        <h5>{{ trans('orders.options') }}</h5>
+        <a class="small expanded success button" href="#"><i class="fa fa-copy"></i> {{ trans('orders.copy') }}</a>
+        <a class="small expanded warning button" href="#"><i class="fa fa-pencil"></i> {{ trans('orders.edit') }}</a>
+        <a class="small expanded alert button" href="#"><i class="fa fa-trash"></i> {{ trans('orders.cancel') }}</a>
       </div>
       <div class="callout">
-        <h5>Remarks</h5>
+        <h5>{{ trans('orders.remarks') }}</h5>
         @foreach($pallet->get_remarks() as $remark)
-          <div class="callout secondary">{{ $remark->body }} <em>{{ date('d.m.Y', time($remark->created_at)) }} at {{ date('H:i:s', time($remark->created_at)) }}</em></div>
+          <div class="callout secondary">{{ $remark->body }} <em>{{ date('d.m.Y', time($remark->created_at)) }} {{ trans('orders.at') }} {{ date('H:i:s', time($remark->created_at)) }}</em></div>
         @endforeach
-        <h6>New remark:</h6>
-        <textarea type="text" name="remark" cols="20" rows="2" placeholder="Do you have any remarks regarding this order?"></textarea>
-        <button class="button">Add remark</button>
+        <h6>{{ trans('orders.newremark') }}:</h6>
+        <textarea type="text" name="remark" cols="20" rows="2" placeholder="{{ trans('orders.remarkdesc') }}"></textarea>
+        <button class="button">{{ trans('orders.addremark') }}</button>
       </div>
     </div>
 
