e4db7afefeabe667c5f0a5e933a22dbd6357399d
Robert Schlick <robert.schlick@online.de>
2016-02-18 15:29:25 +0200

    Bugfix pallet order actions, container options view and model

---
+++ database/migrations/2016_02_17_202016_create_options_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('options', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('identity_id')->default(0);
+            $table->double('value',2)->default(0.00);
+            $table->integer('counter')->default(1);
+            $table->string('orderReference')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('options');
+    }
+}
--- public/js/orderactions.js
+++ public/js/orderactions.js
@@ -17,42 +17,30 @@ $(document).ready(function() {
 
 	// Functionality for cancelling orders:
 	$('#table').on('click', '.cancelOrderModalButton', function() {
-		var reference = $(this).attr('orderReference');
-		$('#orderReference').val(reference);
-		$('.orderReferenceSpan').text(reference);
+		cancelOrderModal(this);
+	});
+
+	$('.cancelOrderModalButton').on('click', null, function() {
+		cancelOrderModal(this);
 	});
 
 	// Functionality for copying orders:
-	$('#table').on('click', '.copyOrderModalButtonn', function() {
-		$('#orderModalHeadline').text('Copy a previous order');
-		$('#orderModalButton').text('Place Order');
-		var reference = $(this).attr('orderReference');
-	    var request = $.get('pallets/' + reference + '/get');
-	    request.done(function(response) {
-	    	//console.log(response);
-	    	$.each(response, function(k, v) {
-			    $('#order_'+k).val(v);
-			});
-			calculatePrice('#modalPriceTotal','.modalOrderPalletOption');
-	    });
+	$('#table').on('click', '.copyOrderModalButton', function() {
+		copyOrderModal(this);
+	});
+
+	$('.copyOrderModalButton').on('click', null, function() {
+		copyOrderModal(this);
 	});
 
 	// Functionality for editing orders:
 	$('#table').on('click', '.editOrderModalButton', function() {
-    	$('#orderModalHeadline').text('Edit an order');
-		$('#putOrderModal').html('<input type="hidden" name="_method" value="PUT">');
-		$('#orderModalButton').text('Save Changes');
-    	var reference = $(this).attr('orderReference');
-	    var request = $.get('pallets/' + reference + '/get');
-	    request.done(function(response) {
-	    	//console.log(response);
-	    	$.each(response, function(k, v) {
-			    $('#order_'+k).val(v);
-			});
-			$('#order_remark').val('');
-			calculatePrice('#modalPriceTotal','.modalOrderPalletOption');
-	    });
+    	editOrderModal(this);
     });
+
+    $('.editOrderModalButton').on('click', null, function() {
+		editOrderModal(this);
+	});
 });
 
 function calculatePrice(targetObject, sourceClass){
@@ -62,4 +50,38 @@ function calculatePrice(targetObject, sourceClass){
     });
     sum = sum.toFixed(2);
     $(targetObject).text(sum);
+}
+
+function editOrderModal(obj){
+	$('#orderModalHeadline').text('Edit an order');
+	$('#putOrderModal').html('<input type="hidden" name="_method" value="PUT">');
+	$('#orderModalButton').text('Save Changes');
+    var reference = $(obj).attr('orderReference');
+	var request = $.get('pallets/' + reference + '/get');
+	request.done(function(response) {
+	    $.each(response, function(k, v) {
+			$('#order_'+k).val(v);
+		});
+		$('#order_remark').val('');
+		calculatePrice('#modalPriceTotal','.modalOrderPalletOption');
+	});	
+}
+
+function copyOrderModal(obj){
+	$('#orderModalHeadline').text('Copy a previous order');
+	$('#orderModalButton').text('Place Order');
+	var reference = $(obj).attr('orderReference');
+	var request = $.get('pallets/' + reference + '/get');
+	request.done(function(response) {
+		$.each(response, function(k, v) {
+			$('#order_'+k).val(v);
+		});
+		calculatePrice('#modalPriceTotal','.modalOrderPalletOption');
+	});
+}
+
+function cancelOrderModal(obj){
+	var reference = $(obj).attr('orderReference');
+	$('#orderReference').val(reference);
+	$('.orderReferenceSpan').text(reference);
 }
