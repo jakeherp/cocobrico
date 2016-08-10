e7663ec2089d97cf49dfbca3d787e4561fec9545
Christian Gitter <christiangitter@gmail.com>
2016-08-10 21:45:00 +0200

    update model-factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,37 @@
 |
 */
 
+$factory->define(App\Address::class, function (Faker\Generator $faker) {
+	return [
+		'type' => $faker->numberBetween(1,3),
+		'companyName' => $faker->company,
+		'firstName' => $faker->firstName,
+		'lastName' => $faker->lastName,
+		'address1' => $faker->streetAddress,
+		'city' => $faker->city,
+		'postCode' => $faker->postcode,
+		'country_id' => $faker->numberBetween(1,253),
+		'phone' => $faker->phoneNumber,
+		'fax' => $faker->phoneNumber,
+		'email' => $faker->safeEmail,
+	];
+});
+
 $factory->define(App\User::class, function (Faker\Generator $faker) {
-    return [
-        'name' => $faker->name,
-        'email' => $faker->email,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
-    ];
+	return [
+		'firstname' => $faker->firstName,
+		'lastname' => $faker->lastName,
+		'email' => $faker->email,
+		'password' => bcrypt(str_random(10)),
+		'remember_token' => str_random(10),
+	];
+});
+
+$factory->define(App\Warehouse::class, function (Faker\Generator $faker) {
+	return [
+		'name' => $faker->name,
+		'location' => $faker->city,
+		'physicalStock' => $faker->randomDigit,
+		'sellableStock' => $faker->randomDigit
+	];
 });
